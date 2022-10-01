11ccab1897b448696f7a55297b3ae45432721dde
Joaquin Franich <joaquinmelo90@gmail.com>
2022-10-01 18:45:04 -0300

    Cambios en package.json para deploy en gh-pages.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './context/UserContext';
-import { ColorPalettesProvider } from './context/ColorPalettesContext';
-import { FavoritesProvider } from './context/FavoritesContext';
-import { FiltersProvider } from './context/FiltersContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { HashRouter } from "react-router-dom";
+import { UserProvider } from "./context/UserContext";
+import { ColorPalettesProvider } from "./context/ColorPalettesContext";
+import { FavoritesProvider } from "./context/FavoritesContext";
+import { FiltersProvider } from "./context/FiltersContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <UserProvider>
         <ColorPalettesProvider>
           <FavoritesProvider>
             <FiltersProvider>
-                <App /> 
+              <App />
             </FiltersProvider>
           </FavoritesProvider>
         </ColorPalettesProvider>
       </UserProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
